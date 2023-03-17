c757dc876ab1488143cba86c493c096dc4b2b483
Thomas Raphael de Oliveira <thoraph96@gmail.com>
2023-03-17 19:06:43 -0300

    update aula 13 à 17
    aula 13 à 17

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 # instanciando a classe Flask
 app = Flask(__name__)
 
 lista_usuarios = ['Thomas','Evelyn','Marcus','Johnny']
+
+app.config['SECRET_KEY'] = 'b480e6cf1f889e49d122f1a7de513f84'
 
 # decorator é uma função que começa com um @ 
 # e sempre vem antes de uma função para atribuir uma nova funcionalidade à ela
@@ -19,6 +22,11 @@ def contato():
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
+@app.route('/login')
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login = form_login, form_criarconta = form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
