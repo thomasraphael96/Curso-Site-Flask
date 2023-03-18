1242e42cbd3bff65b76d385bc0b41001eca4a0bb
Thomas Raphael de Oliveira <thoraph96@gmail.com>
2023-03-18 12:29:07 -0300

    update aula 18 à 20
    - Melhorando o visual dos formulários com Bootstrap.
    - Liberando o método POST da página de Login
    - Token de segurança (csrf) e mensagem de sucesso nos formulários

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 # instanciando a classe Flask
@@ -22,10 +22,21 @@ def contato():
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-@app.route('/login')
+# liberar o método POST para ser possível enviar informações para o site (utilizado nos forms). Ele vem bloqueado por segurança
+# método GET já vem liberado por padrão
+@app.route('/login', methods=['GET','POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        #fez login com sucesso
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        #criou conta com sucesso
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', form_login = form_login, form_criarconta = form_criarconta)
 
 if __name__ == '__main__':
