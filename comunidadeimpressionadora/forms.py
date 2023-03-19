05fe01dc111ab415c4c0e653e27b1a3d6214db7f
Thomas Raphael de Oliveira <thoraph96@gmail.com>
2023-03-19 20:07:48 -0300

    Update aula 35 à 36
    35. Carregando imagem padrão de perfil
    36. Página de edição de perfil

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -20,3 +20,8 @@ class FormLogin(FlaskForm):
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
     lembrar_dados = BooleanField('Lembrar dados de acesso')
     botao_submit_login = SubmitField('Fazer Login')
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    botao_submit_editarperfil = SubmitField('Confirmar Edição')
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, request, flash, redirect
-from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.models import Usuario
 from flask_login import login_user, logout_user, current_user, login_required
@@ -63,9 +63,17 @@ def sair():
 @app.route('/perfil')
 @login_required
 def perfil():
-    return render_template('perfil.html')
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil = foto_perfil)
 
 @app.route('/post/criar')
 @login_required
 def criar_post():
     return render_template('criarpost.html')
+
+@app.route('/perfil/editar', methods=['GET','POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil = foto_perfil, form = form)
