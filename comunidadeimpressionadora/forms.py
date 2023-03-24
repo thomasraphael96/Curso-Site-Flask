c4261ba63fc32d73a53d2a7c50c421f2c6a31d39
Thomas Raphael de Oliveira <thoraph96@gmail.com>
2023-03-24 18:41:01 -0300

    Update aula 44
    44. Permitindo a criação de posts

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadeimpressionadora.models import Usuario
 from flask_login import current_user
@@ -40,4 +40,8 @@ class FormEditarPerfil(FlaskForm):
             usuario = Usuario.query.filter_by(email=email.data).first()
             if usuario:
                 raise ValidationError('Já existe um usuário com esse e-mail.')
-    
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do Post', validators=[DataRequired(), Length(2,140)])
+    corpo = TextAreaField('Escreva seu post aqui', validators=[DataRequired()])
+    botao_submit_criarpost = SubmitField('Criar Post')
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, request, flash, redirect
-from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora import app, database, bcrypt
-from comunidadeimpressionadora.models import Usuario
+from comunidadeimpressionadora.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
@@ -70,10 +70,17 @@ def perfil():
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('perfil.html', foto_perfil = foto_perfil)
 
-@app.route('/post/criar')
+@app.route('/post/criar', methods=['GET','POST'])
 @login_required
 def criar_post():
-    return render_template('criarpost.html')
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo = form.titulo.data, corpo = form.corpo.data, autor = current_user)
+        database.session.add(post)
+        database.session.commit()
+        flash('Post criado com sucesso', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
 
 
 def salvar_imagem(imagem):
