1544e0330faf047c01ae8911e7612f35a5b6caa8
Thomas Raphael de Oliveira <thoraph96@gmail.com>
2023-03-25 18:58:56 -0300

    Update aula 51
    51. Método de contagem de posts

--- comunidadeimpressionadora/models.py
+++ comunidadeimpressionadora/models.py
@@ -16,6 +16,9 @@ class Usuario(database.Model, UserMixin):
     posts = database.relationship('Post', backref = 'autor', lazy = True)
     cursos = database.Column(database.String, nullable = False, default = 'Não informado')
 
+    def contar_posts(self):
+        return len(self.posts)
+
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key = True)
     titulo = database.Column(database.String, nullable = False)
